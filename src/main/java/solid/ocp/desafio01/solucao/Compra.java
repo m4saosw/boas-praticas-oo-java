e9a817af08ffd86c93287dd0408695b8a511e925
Massao <lmassao@yahoo.com.br>
2021-04-21 19:20:51 -0300

    Solucao desafio 01 - open closed principle - inicio

---
+++ src/main/java/solid/ocp/desafio01/solucao/Compra.java
@@ -0,0 +1,14 @@
+package solid.ocp.desafio01.solucao;
+
+public class Compra {
+	private double valor;
+	private String cidade;
+	public double getValor() {
+		return valor;
+	}
+	public String getCidade() {
+		return cidade;
+	}
+	
+	
+}
