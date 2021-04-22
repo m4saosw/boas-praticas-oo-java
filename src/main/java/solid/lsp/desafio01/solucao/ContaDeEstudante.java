778e2e11c93b163df55c2f21d62a014f2e24a0d8
Massao <lmassao@yahoo.com.br>
2021-04-21 23:07:01 -0300

    Solucao desafio 01 - liskov substitutive principle - inicio

---
+++ src/main/java/solid/lsp/desafio01/solucao/ContaDeEstudante.java
@@ -0,0 +1,19 @@
+package solid.lsp.desafio01.solucao;
+
+public class ContaDeEstudante extends ContaComum {
+
+    private int milhas;
+
+    public void deposita(double valor) {
+        super.deposita(valor);
+        this.milhas += (int)valor;
+    }
+
+    public int getMilhas() {
+        return milhas;
+    }
+
+    public void rende() {
+        throw new RuntimeException("Não pode render");
+    }
+}
